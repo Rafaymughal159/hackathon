5d4d1799521961b7e36aaaaebd409dd11545c1a3
rafay mughal <kingking8753@gmail.com>
2024-12-17 00:15:57 +0800

    first commit

---
+++ src/app/product/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NewsletterInstagram from '../components/instagram'
+import AllProduct from '../components/allProducts'
+
+const page = () => {
+  return (
+      <div>
+          
+          <AllProduct />
+          <NewsletterInstagram />
+      
+    </div>
+  )
+}
+
+export default page
